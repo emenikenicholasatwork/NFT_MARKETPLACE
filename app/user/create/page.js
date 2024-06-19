e616ffdc80051d9db9ba4e79aaa24674d903e5e7
emenikenicholasatwork <emenikenicholasatwork@gmail.com>
2024-06-19 14:12:36 +0100

    another commit

--- app/user/create/page.js
+++ app/user/create/page.js
@@ -1,7 +1,49 @@
+"use client"
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
+
 const page = () => {
+  const [imagePreview, setImagePreview] = useState("");
+  const imageRef = useRef();
+
+  const handle_image_change =()=>{
+    const file = imageRef.current.files[0];
+    if (file) {
+      const validTypes = ['image/png', 'image/svg+xml', 'image/gif', 'image/jpeg'];
+      if (validTypes.includes(file.type)) {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          setImagePreview(reader.result);
+        };
+        reader.readAsDataURL(file);
+      } else {
+        alert('Please select a PNG, SVG, GIF, or JPG image.');
+        fileInputRef.current.value = '';
+      }
+    };
+  };
+  useEffect(()=>{
+    console.log(imagePreview)
+  },[imagePreview]);
+  
   return (
     <main className="min-h-screen flex py-5 justify-center">
-      <h1>It's time to own my own NFT</h1>
+      <h1 className="text-lg font-bold">It's time to own an NFT</h1>
+      <div>
+        <div>
+          <label htmlFor="">NFT logo image</label>
+          <div className="flex flex-row gap-5 items-center border-[1px] h-60 rounded-lg border-black">
+            <div>
+              <Image name="nft_image" alt="nft image" src={imagePreview} onClick={()=>imageRef.current.click()} className="w-56 h-56 rounded-lg shadow-lg cursor-pointer" height={50} width={50} /> 
+              <input className="hidden" type="file" accept=".png, .svg, .gif, .jpg, .jpeg, image/png, image/svg+xml, image/gif, image/jpeg" onChange={handle_image_change} ref={imageRef}/>
+            </div>
+            <div>
+              <p>Drag and drop or click to upload</p>
+              <p>Recommended File Type: JPG, SVG, PNG, GIF</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
--- components/Login.jsx
+++ components/Login.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { useGlobal } from '@/context/GlobalContext'
 import { ethers } from 'ethers'
+import {CiWallet} from 'react-icons/ci'
 import React, { useState } from 'react'
 import { toast, ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
@@ -10,10 +11,10 @@ const Login = () => {
   const [walletAddress, setWalletAddress] = useState(null)
   const notify =(message) => toast(message);
   const {isNightMode, setLogin, activate_account} = useGlobal()
-
-  const connect_metamask = async()=>{
+  ethers.Provider
+  const connect_wallet = async()=>{
     try{
-      if(typeof window.ethereum !== 'undefined' && window.ethereum.isMetaMask){
+      if(typeof window.ethereum !== 'undefined'){
         const account = await window.ethereum.request({method: 'eth_requestAccounts'})
         if(account.length){
           activate_account(account[0])
@@ -23,36 +24,16 @@ const Login = () => {
         // const provider = typeof window.ethereum !== 'undefined' ? new ethers.providers.Web3Provider(window.ethereum) : null;
         console.log(window.ethereum)
       }else {
-        notify('MetaMask is not installed.');
+        notify('No wallet installed. Kindly install any crypto or web3 wallet.. eg metamask..');
       };
     }catch(error){
       console.log(`Error while connecting metamask: ${error}`);
     };
   };
       
-        const connect_coinbase = () => {
-          if (typeof window.ethereum !== 'undefined' && window.ethereum.isCoinbaseWallet) {
-          } else {
-            notify('Coinbase Wallet is not installed.');
-          }
-        };
-      
-        const connect_phantom = async () => {
-          if (typeof window.solana !== 'undefined' && window.solana.isPhantom) {
-            try{
-              const sol = await window.solana.connect();
-              const account = sol.publicKey.toString();
-              console.log(account)
-            }catch(error){
-              console.error('User denied account access or there was an error:', error);
-            }
-          } else {
-            notify('Phantom Wallet is not installed.');
-          }
-        };
   return (
     <main className='fixed z-50 top-0 right-0 w-full h-full flex items-center justify-center bg-blurBackground font-bold'>
-        <div className={`h-[550px] mb-32 w-[400px] ${isNightMode ? 'bg-black text-white' : 'bg-white text-black'} pt-10 rounded-lg shadow-md flex flex-col  relative px-2`}>
+        <div className={`h-[450px] mb-32 w-[400px] ${isNightMode ? 'bg-black text-white' : 'bg-white text-black'} pt-10 rounded-lg shadow-md flex flex-col  relative px-2`}>
         <i className='bi bi-x-lg absolute right-7 top-7 text-lg font-extrabold cursor-pointer' onClick={setLogin}></i>
             <div className='flex flex-col items-center font-bold gap-2 mt-5'>
                 <i className='bi bi-currency-bitcoin text-4xl bg-green-500 p-3 rounded-md'></i>
@@ -60,19 +41,9 @@ const Login = () => {
             </div>
             <div className='w-full flex flex-col items-center mt-10'>
                 <ul className='bg-[#938c8c88] w-[380px] overflow-hidden flex flex-col rounded-lg shadow-xl'>
-                    <li className='flex flex-row items-center p-3 gap-3 cursor-pointer hover:bg-[#9e8c8c75]' onClick={connect_metamask}>
-                        <img src="/icons/metamask.svg" height={20} width={30} alt="MetaMask Icon" />
-                        <p>MetaMask</p>
-                    </li>
-                    <hr className='w-[380px]'/>
-                    <li className='flex flex-row items-center p-3 gap-3 cursor-pointer hover:bg-[#9e8c8c75]' onClick={connect_phantom}>
-                        <img src="/icons/Phantom-Icon_App.svg" height={20} width={30} alt="Phantom Icon" />
-                        <p>Phantom</p>
-                    </li>
-                    <hr className='w-[380px]'/>
-                    <li className='flex flex-row items-center p-3 gap-3 cursor-pointer hover:bg-[#9e8c8c75]' onClick={connect_coinbase}>
-                        <img src="/icons/coinbase.svg" height={20} width={30} alt="Phantom Icon" />
-                        <p>Coinbase Wallet</p>
+                    <li className='flex flex-row items-center p-3 gap-3 cursor-pointer hover:bg-[#9e8c8c75]' onClick={connect_wallet}>
+                      <CiWallet className='text-xl'/>
+                      <p>Connect Wallet</p>
                     </li>
                 </ul>
             </div>
