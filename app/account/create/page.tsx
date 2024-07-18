31a99b2d983a2cf4ae02a398c2d45e61b571a289
Nicholas Emenike <nicholasemenikeatwork@gmail.com>
2024-07-18 17:56:52 +0100

    making small changes 2

--- app/account/create/page.tsx
+++ app/account/create/page.tsx
@@ -2,13 +2,40 @@
 import { useGlobal } from "../../../context/GlobalContext";
 import * as React from "react";
 import { useEffect, useRef, useState } from "react";
+import nftData from "../../../components/collections/nft.json";
+import { BsCollection } from "react-icons/bs";
 
 const Page: React.FC = () => {
   const { isNightMode } = useGlobal();
   const [imagePreview, setImagePreview] = useState<string | ArrayBuffer | null>(null);
   const imageRef = useRef<HTMLInputElement>(null);
   const [imageEmpty, setImageEmpty] = useState(true);
-  const [collectCreateMode, setCollectionCreateMode] = useState(true);
+  const [showAllCollections, setShowAllCollections] = useState(false);
+  const [groupedNfts, setGroupedNfts] = useState<{ [key: string]: NFT[] }>({});
+  const [openNewCollection, setOpenNewCollection] = useState(false);
+  const [newCollName, setNewCollName] = useState("Select a collection");
+  const [nftToMintData, setNftToMintData] = useState({
+    name: "",
+    description: "",
+    image: "",
+    collection: "",
+    price: ""
+  })
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setNftToMintData((prevData) => ({
+      ...prevData,
+      name: value,
+    }));
+    console.log(nftToMintData)
+  };
+
+  interface NFT {
+    id: number;
+    name: string;
+    collection: string;
+  }
 
   const imageButton = () => {
     if (imageRef.current) {
@@ -38,8 +65,18 @@ const Page: React.FC = () => {
   };
 
   useEffect(() => {
-    console.log(imagePreview);
-  }, [imagePreview]);
+    const groupByCollection = (nfts: NFT[]): { [key: string]: NFT[] } => {
+      return nfts.reduce((acc, nft) => {
+        const { collection } = nft;
+        if (!acc[collection]) {
+          acc[collection] = [];
+        }
+        acc[collection].push(nft);
+        return acc;
+      }, {} as { [key: string]: NFT[] });
+    };
+    setGroupedNfts(groupByCollection(nftData));
+  }, []);
 
   return (
     <main className={`min-h-screen overflow-auto pt-20 w-full flex flex-col py-5 justify-center items-center`}>
@@ -59,33 +96,48 @@ const Page: React.FC = () => {
                     <p className="font-thin">JPG, PNG, GIF, SVG</p>
                   </div>
                   :
-                  <div className="w-[400px] h-[400px]  lg:w-[600px] lg:h-[600px] rounded-md">
-                    <img alt="nft image" src={typeof imagePreview === 'string' ? imagePreview : ''} onClick={imageButton} className="w-[600px] h-[600px] hover:scale-[1.01] duration-300 rounded-lg shadow-lg cursor-pointer" />
+                  <div className="w-[350px] h-[350px]  md:w-[600px] md:h-[600px] rounded-md">
+                    <img alt="nft image" src={typeof imagePreview === 'string' ? imagePreview : ''} onClick={imageButton} className="md:w-[600px] md:h-[600px] h-[350px] w-[350px] hover:scale-[1.01] duration-300 rounded-lg shadow-lg cursor-pointer" />
                   </div>
               }
               <input className="hidden" type="file" accept=".png, .svg, .gif, .jpg, .jpeg, image/png, image/svg+xml, image/gif, image/jpeg" onChange={handleImageChange} ref={imageRef} />
             </div>
           </div>
           <div className="flex flex-col flex-1 w-[330px] md:w-[600px] gap-3">
-            <div className="flex flex-col w-full gap-2">
+            <div className="flex flex-col w-full gap-2 relative">
               <p>Collection *</p>
-              <div className={`flex flex-row items-center gap-3 border border-slate-600 duration-200 w-full p-5 rounded-md cursor-pointer `}>
-                <p className={`py-5 px-7 text-2xl rounded-md ${isNightMode ? "bg-slate-700" : "bg-slate-300"}`}>+</p>
-                <p>Create new collection</p>
+              <div className={`flex flex-row items-center gap-3 border border-slate-600 duration-200 w-full p-5 rounded-md`}>
+                <p className={`py-5 px-7 text-2xl rounded-md ${isNightMode ? "bg-slate-700" : "bg-slate-300"}`}><BsCollection /></p>
+                <p>{newCollName}</p>
               </div>
               <div className="text-end text-blue-600">
                 {
-                  collectCreateMode ? <p className="cursor-pointer">show available collections</p> : <p className="cursor-pointer">create new collection</p>
+                  openNewCollection ? <p className="cursor-pointer" onClick={() => { setShowAllCollections(!showAllCollections); }}>show available collections</p> : <p className="cursor-pointer" onClick={() => setOpenNewCollection(!openNewCollection)}>create new collection</p>
                 }
               </div>
+              {showAllCollections &&
+                <ul className={`flex flex-col overflow-auto p-5 absolute left-10 top-10 ${isNightMode ? "bg-[#252927] text-white" : "bg-white text-black"} duration-300 `}>
+                  {
+                    Object.keys(groupedNfts).map((collectionName) => (
+                      <li className="cursor-pointer hover:bg-[#9e8c8c] p-3 rounded-md" onClick={() => { setShowAllCollections(!showAllCollections); setNewCollName(collectionName); setOpenNewCollection(!openNewCollection) }}>
+                        {collectionName}
+                      </li>
+                    ))
+                  }
+                </ul>
+              }
             </div>
             <div className="flex flex-col gap-1">
               <p>Name *</p>
-              <input className="rounded-md bg-transparent border p-2 border-slate-600" type="text" placeholder="Name your NFT" />
+              <input className="rounded-md bg-transparent border p-2 border-slate-600" type="text" placeholder="Name your NFT" name="name" />
+            </div>
+            <div className={`duration-700 ease-in-out ${(openNewCollection && !showAllCollections) ? "h-[80px] " : "h-0"} flex flex-col gap-1 overflow-hidden`}>
+              <p>Collections *</p>
+              <input className="rounded-md bg-transparent border p-2 border-slate-600" type="text" placeholder="Name your Collection" onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNewCollName(e.target.value)} />
             </div>
             <div className="flex flex-col gap-1">
               <p>Description *</p>
-              <textarea className="rounded-md border border-slate-600 bg-transparent resize-none p-2 h-28" placeholder="Enter a description"></textarea>
+              <textarea className="rounded-md border border-slate-600 bg-transparent resize-none p-2 h-28" placeholder="Enter a description" onChange={() => onInputChange}></textarea>
             </div>
             <div className="flex flex-col gap-1">
               <p>Price (in ETH)</p>
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,45 +1,29 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <footer className='bg-blue-700 min-h-fit px-10 z-20 pt-10 text-white'>
-        <div className='flex lg:gap-36 lg:flex-row flex-col items-center w-full'>
-        <div className='flex lg:flex-row gap-5 lg:gap-0 flex-col '>
-            <div className='flex flex-col gap-2 items-center lg:items-start flex-1'>
-                <i className="bi bi-currency-bitcoin text-3xl bg-green-500 p-2 rounded-md flex w-12"></i>
-                <p className='font-bold'>Cryto~Art</p>
-            </div>
-        </div>
-            <div className='flex-1 flex flex-col gap-3'>
+    return (
+        <footer className='bg-blue-700 min-h-fit px-10 z-20 pt-10 text-white'>
+            <div className='flex lg:gap-36 lg:flex-row flex-col items-center w-full'>
+                <div className='flex lg:flex-row gap-5 lg:gap-0 flex-col '>
+                    <div className='flex flex-col gap-2 items-center lg:items-start flex-1'>
+                        <i className="bi bi-currency-bitcoin text-3xl bg-green-500 p-2 rounded-md flex w-12"></i>
+                        <p className='font-bold'>Cryto~Art</p>
+                    </div>
+                </div>
+                <div className='flex-1 flex flex-col gap-3'>
                     <p className='text-sm'>Join our mailing list to stay in the loop with our newest feature releases, NFT drops, and tips and tricks for navigating Crypto~Art</p>
-                <div className='items-center flex flex-row gap-2'>
-                    <input type="text" className='p-2 rounded-lg flex-1 outline-none text-black' placeholder='Your email address'/>
-                    <button className='lg:px-5 p-3 lg:py-2 bg-blue-500 rounded-lg hover:bg-blue-900 duration-200'>Sign up</button>
                 </div>
             </div>
-            <div className='flex-1 gap-2 flex flex-col items-center'>
-                <p className='font-bold'>Join the community</p>
-                <div className='flex flex-row gap-3 flex-wrap'>
-                    <i className='bi bi-twitter bg-blue-500 p-3 rounded-lg cursor-pointer hover:bg-blue-900 duration-200'></i>
-                    <i className='bi bi-instagram bg-blue-500 p-3 rounded-lg cursor-pointer hover:bg-blue-900 duration-200'></i>
-                    <i className='bi bi-discord bg-blue-500 p-3 rounded-lg cursor-pointer hover:bg-blue-900 duration-200'></i>
-                    <i className='bi bi-whatsapp bg-blue-500 p-3 rounded-lg cursor-pointer hover:bg-blue-900 duration-200'></i>
-                    <i className='bi bi-youtube bg-blue-500 p-3 rounded-lg cursor-pointer hover:bg-blue-900 duration-200'></i>
-                    <i className='bi bi-tiktok bg-blue-500 p-3 rounded-lg cursor-pointer hover:bg-blue-900 duration-200'></i>
-                    <i className='bi bi-envelope-at bg-blue-500 p-3 rounded-lg cursor-pointer hover:bg-blue-900 duration-200'></i>
+            <hr className='mt-6 mb-3' />
+            <div className='font-light w-full flex flex-row justify-between items-center min-w-fit lg:text-lg text-sm'>
+                <p>Copyright &copy; 2024 by Nicholas || All Right Reserved.</p>
+                <div className='flex flex-row gap-5'>
+                    <p>Privacy Policy</p>
+                    <p>Terms of Service</p>
                 </div>
             </div>
-        </div>
-        <hr className='mt-6 mb-3'/>
-        <div className='font-light w-full flex flex-row justify-between items-center min-w-fit'>
-            <p>Copyright &copy; 2024 by Nicholas || All Right Reserved.</p>
-            <div className='flex flex-row gap-5'>
-                <p>Privacy Policy</p>
-                <p>Terms of Service</p>
-            </div>
-        </div>
-    </footer>
-  )
+        </footer>
+    )
 }
 
 export default Footer
